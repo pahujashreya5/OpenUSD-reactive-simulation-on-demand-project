a812555530a96ad1b167f1db5e8d57999e47bbdb
pahujashreya5 <pahujashreya5@gmail.com>
2026-07-19 18:33:27 +0530

    final houdiniWorker

--- python-scripts/houdini_worker.py
+++ python-scripts/houdini_worker.py
@@ -4,137 +4,149 @@
 import sys, argparse, os
 from pxr import Usd, Sdf
 print(Sdf.FileFormat.FindById('usdnc'))
-print("usd version: ", Usd.GetVersion())
+print('usd version: ', Usd.GetVersion())
 
 try:
-    import hou # the houdini library. hython has access to this because it is houdini's compiler. this will only work if this script is run using the hython compiler. python will throw error because it does not know this library.
-    # although the hom docs give a way of running this using standard python interpreter: https://www.sidefx.com/docs/houdini/hom/cb/hipfile.html
-    print("imported hou")
+    import hou 
+    print('imported hou')
 except ImportError:
-    print("make sure to  you are using HYTHON and NOT standard PYTHON interpreter.")
+    print('make sure to  you are using HYTHON and NOT standard PYTHON interpreter.')
     sys.exit(1) # we tell the subprocess to shut down immediately
 
-def DustSimulationBuildAndExport(frame, x, y, z):
-    # check this before doing anything else
-    # CheckHDALoaded()
-    print("starting building sim")
-    # now we use python script to create the dust sim https://www.sidefx.com/docs/houdini/hom/hou/index.html
-
-    # we need to set the timeline so that sim is spawned at hit frame and we decide its life
-    start_frame=frame # which we got from our dispatch file output
-    life=45 # we can tweak this according to how the dust looks finally
-    end_frame=frame+life
-    hou.playbar.setFrameRange(start_frame, end_frame) # we just set the start and end for the timeline that you see at the bottom of houdini's GUI. see animation section of the HOU docs
-
-    # we create a houdini object so that it does the simulation work for us
-    obj_context=hou.node('/obj')
-    # we tell the houdini object to create a new node in which we will create a node network for a dust simulation. the type is geometry and the name is dust
-    dust_node=obj_context.createNode('geo', 'dust')
-    # let's create the node which will emit dust
-    # this means: we enter the dust geo root node and create a node called emitter. it is just a sphere
-    emitter=dust_node.createNode('sphere', 'emitter_geom')
-    # this is the source of our dust particles and we need it to be at the hit contact point
-    emitter.parm('tx').set(x)
-    emitter.parm('ty').set(y)
-    emitter.parm('tz').set(z)
-    size=0.2 # we want tiny dust particles
-    emitter.parm('scale').set(size)
-
-    print("starting popnet building")
-
-    # refer to note 1 in readme in case of node not recognized error
-    # now we create the particle network (popnet)
-    popnet=dust_node.createNode('dopnet', 'dust_simulation') # we add a popnet node to the network and call it dust_simulation
-    # popnet=dopnet.createNode('popnet')  
-    # now we need to use the particles that we created before as input
-    print("popnet node added")
-    popnet.setInput(0, emitter) # tells houdini object to connect the emitter to the first(0) input of our popnet node
-
-    popsource=popnet.createNode('popsource', 'src_particles') # access the source node natively living inside houdini
-    # print("popsource node created", popsource)
-    # now we set some channel values and attributes to make the particles behave like dust being emitted
-    popsource.parm('constantactivate').set(0) # we dont want infinite stream of particles so set it to off
-    # print("set const activation off")
+def DustSimulationBuildAndExport(export_file_path):
+    print('starting building simple test sim')
     
-    # print("set count of popsource impulse")
-    # just add an hscript expression into the channel where we define conditions for the sim. we need it to begin when frame is start_frame
-    popsource.parm('impulseactiveate').setExpression(f"$F=={start_frame}")
-    count=1000 # let's create 1000 particles as dust for now
-    popsource.parm('impulserate').set(count) 
-    # print("set start frame for impulse activation")
-    # now we dont want them to just be created and sit there so we add an intial velocity (it should look like impact dust when there is a collision)
-    print("popnet setup done")
-    vel=3.0
-    # variance is the name of the attribute that houdini uses for this. let's set all x y and z direction velocities to 3.0 for now
-    popsource.parm('velx').set(vel)
-    popsource.parm('vely').set(vel)
-    popsource.parm('velz').set(vel)
+    start_frame = 1 
+    life = 45 
+    end_frame = start_frame + life
+    hou.playbar.setFrameRange(start_frame, end_frame) 
 
-    # let's print something to help with debugging later
-    print("dust sim created successfully by houdini. proceeding to export via usd")
+    obj_context = hou.node('/obj')
+    dust_node = obj_context.createNode('geo', 'dust_sim_gen')
+    # dust_node.clearChildren()
+    
+    # test_sphere = dust_node.createNode('sphere', 'test_mesh')
+    # test_sphere.parm('type').set(1) # Convert to polygon
+    
+    # test_sphere.parm('scale').setExpression(f'max(0, ($F - {start_frame}) * 0.15)')
 
-    # now let's work on exporting this. we need to use solaris so that it takes the dust simulation that houdini just built, converts it into lightweight USD format and then sends it to our os so that we can do the remaining steps with python script. (basically, we are aiming to store the lightweight sim until we actually have to render it in whichever application you want to. i will use a usd payload, but more on that later)
+    # output_null = dust_node.createNode('null', 'OUT_particles')
+    # output_null.setInput(0, test_sphere) 
 
-    # we need to change from object/geometry context to solaris context
-    stage_context=hou.node('/stage') # switch from build to solaris
-    # import the sim into our usd stage using a sop
+    emitter_source = dust_node.createNode('sphere', 'emitter_source')
+    emitter_source.parm('scale').set(0.1)
+    
+    # B. Scatter 500 points (our dust particles) onto the emitter
+    scatter = dust_node.createNode('scatter', 'emit_points')
+    scatter.setInput(0, emitter_source)
+    scatter.parm('npts').set(500) 
+    
+    # C. Apply VEX Math to simulate explosion, drag, and gravity 
+    # (Notice the double {{ }} to prevent Python f-string errors!)
+    wrangle = dust_node.createNode('attribwrangle', 'particle_kinematics')
+    wrangle.setInput(0, scatter)
+    vex_code = f"""
+    float age = (@Frame - {start_frame}) / 24.0; // Age in seconds
+    
+    if (age > 0) {{
+        // 1. Generate a random outward direction with an upward bias
+        vector dir = normalize(set(rand(@ptnum)-0.5, rand(@ptnum+10)*0.8, rand(@ptnum+20)-0.5));
+        
+        // 2. Randomize speed per particle
+        float speed = rand(@ptnum+30) * 8.0 + 2.0; 
+        
+        // 3. Air Resistance (Drag): Particles explode fast but slow down quickly in the air
+        float drag = 4.0; 
+        float damped_age = (1.0 - exp(-drag * age)) / drag; 
+        
+        // Apply velocity over time
+        @P += dir * speed * damped_age;
+        
+        // 4. Light gravity gently pulling them back down
+        @P.y -= 0.5 * 1.5 * age * age;
+        
+        // 5. Floor collision (stops them from falling through the ground)
+        if (@P.y < 0.02) @P.y = 0.02;
+        
+        // 6. Scale them down over time so they shrink into nothing
+        @pscale = max(0.0, 0.04 * (1.0 - (age / 1.5)));
+    }} else {{
+        // Hide particles before the impact frame
+        @pscale = 0.0;
+    }}
+    """
+    wrangle.parm('snippet').set(vex_code)
+    
+    # D. Create actual geometry for the particles to render properly in USD
+    particle_mesh = dust_node.createNode('sphere', 'particle_mesh')
+    particle_mesh.parm('type').set(1) # Polygon
+    particle_mesh.parm('scale').set(1.0) # VEX @pscale will control the final size!
+    
+    # E. Copy the geometry onto our moving points
+    copy_pts = dust_node.createNode('copytopoints', 'copy_to_particles')
+    copy_pts.setInput(0, particle_mesh)
+    copy_pts.setInput(1, wrangle)
 
-    print("creating sop import")
-    sop_import=stage_context.createNode('sopimport', 'sop_import_node')
-    print("created sop import")
-    sop_import.parm('soppath').set(popnet.path())
-    print("set soppath to popnet.path()", popnet.path())
-    sop_import.parm('primpath').set('/dust_particles')
-    usd_rop_node=stage_context.createNode('usd_rop', 'usd_rop_node')
+    output_null = dust_node.createNode('null', 'OUT_particles')
+    output_null.setInput(0, copy_pts)
 
-    print("created usd rop node")
-    usd_rop_node.setInput(0, sop_import)
-    print("set usd rop import")
+    # 5. Export via Solaris/USD
+    stage_context = hou.node('/stage') 
 
-    # name of output file
-    out_filename="$HIP/dust_sim.usdnc"
-    # out_filename = hou.text.expandString("$HIP/dust_sim.usdnc")
-    print("created output filename")
-    # set the name. the node is called lopoutput in houdini
-    # actuall .usdnc is the extension for non commercial (apprentice) houdini, which i use. it is identical to .usda, just that it won't work with other DCCs. (will be encrypted for them)
-    usd_rop_node.parm('lopoutput').set(out_filename)
-    print("set output file name")
-    # usd_rop_node.parm('defaultsavestyle').set('usda')
-    usd_rop_node.parm('trange').set(1) # we tell usd that below we will specify frame range to render
-    # set start and end frames for render
+    # CREATE LOPNET FIRST!!!
+    lop_net=stage_context.createNode('subnet', 'dust_export_net')
+    # lop_net.clearChildren()
+
+    # 2. Connect the geometry import underneath it
+    sop_import = lop_net.createNode('sopimport', 'import_dust')
+    sop_import.parm('soppath').set(output_null.path())
+    # sop_import.parm('primpath').set('/dust_asset/mesh') # Local path structure
+    sop_import.parm('pathprefix').set('/dust_asset')
+    # sop_import.parm('enable_setdefaultprim').set(1)
+    # sop_import.parm('setdefaultprim').set(1)
+    config_layer = lop_net.createNode('configurelayer', 'set_metadata')
+    config_layer.setInput(0, sop_import)
+    
+    # Toggle the default prim switch to TRUE (1)
+    config_layer.parm('setdefaultprim').set(1)
+    # Provide the exact matching text path string to the root primitive
+    config_layer.parm('defaultprim').set('/dust_asset')
+
+    usd_rop_node = lop_net.createNode('usd_rop', 'export_usd')
+    usd_rop_node.setInput(0, config_layer)
+    usd_rop_node.parm('lopoutput').set(export_file_path)
+    
+    # Configure the frame range
+    usd_rop_node.parm('trange').set(1) 
     usd_rop_node.parm('f1').set(start_frame) 
     usd_rop_node.parm('f2').set(end_frame)
-    print("set frames")
 
-    # required because otherwise hython doesn;t understand ouput loaction and we would have to specify a layer. but we dont need layers for this simple scene
     usd_rop_node.parm('savestyle').set('flattenstage')
-    # usd_rop_node.parm('erroronsavenodepath').set(0)
 
-    print("starting simulation baking")
-    # usd_rop_node.parm('execute').pressButton() # finally, render!
+    print('starting simulation baking')
     usd_rop_node.render()
-    # print(out_filename.path())
-    print("bake complete 🤯")
+    print('bake complete 🤯')
 
 # to execute the above script
 def main():
-    parser=argparse.ArgumentParser(description="houdini worker")
-    parser.add_argument('--frame', type=int, required=True, help="hit_frame_number")
-    parser.add_argument('--x', type=float, required=True, help="x coord of contact point")
-    parser.add_argument('--y', type=float, required=True, help="y coord of contact point")
-    parser.add_argument('--z', type=float, required=True, help="z coord of contact point")
+    parser=argparse.ArgumentParser(description='houdini worker')
+    parser.add_argument('--frame', type=int, required=True, help='hit_frame_number')
+    parser.add_argument('--x', type=float, required=True, help='x coord of contact point')
+    parser.add_argument('--y', type=float, required=True, help='y coord of contact point')
+    parser.add_argument('--z', type=float, required=True, help='z coord of contact point')
 
     args=parser.parse_args() # store the data in the args variable
 
-    print(f"houdini worker received data {args.frame}, ({args.x}, {args.y}, {args.z})")
+    print(f'houdini worker received data {args.frame}, ({args.x}, {args.y}, {args.z})')
 
-    DustSimulationBuildAndExport(args.frame, args.x, args.y, args.z) 
+    export_file_path = '/Users/shreyapahuja/USD_Projects/dust_sim.usdnc'
+    DustSimulationBuildAndExport(export_file_path)
 
-    print("sim exported. houdini worker shutting down")
+    print('sim exported. houdini worker shutting down')
 
     sys.exit(0) # make sure to shut down the worker so that it doesn't use memory after it has finished its job
 
-if __name__=="__main__":
+if __name__=='__main__':
     main()
 
 
